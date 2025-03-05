0680d2ad8a89123fb08aeee2accddcb695c2b3a4
PauloDiney <paulo19martin@gmail.com>
2025-03-05 18:17:30 -0300

    upDate Moboli Finished

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ $(document).ready(function () {
         $('container-header').toggleClass('blur');
     });
 });
+
+
+
+
+function sendEmail(event) {
+    event.preventDefault();
+    const nome = document.getElementById('nome');
+    const email = document.getElementById('mensagem');
+    const telefone = '5518981104236';
+
+    const texto = `Olá, meu nome é ${nome.value}, gostaria de saber mais sobre seus serviços ${email.value}.`;
+    const link = `https://api.whatsapp.com/send?phone=${telefone}&text=${texto}`;
+
+    window.open(link, '_blank');
+}
